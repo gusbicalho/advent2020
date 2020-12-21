24835111e9a37b3b71154927bbc32d8b69ee4053
Gustavo Bicalho <gusbicalho@gmail.com>
2020-12-21 15:50:10 -0300

    simplify, no need for WriterT, and adjacentCells can be outside the class

--- day17/Main.hs
+++ day17/Main.hs
@@ -19,10 +19,9 @@
 module Main (main) where
 
 import Advent2020.Input qualified
-import Control.Arrow (Arrow ((&&&)))
-import Control.Monad (when)
-import Control.Monad.Trans.Writer.CPS qualified as WriterT
+import Data.Bool qualified as Bool
 import Data.Foldable qualified as Foldable
+import Data.Function ((&))
 import Data.List qualified as List
 import Data.Maybe qualified as Maybe
 import Data.Sequence (Seq)
@@ -48,36 +47,36 @@ solve = Set.size . activeCells . (!! 6) . List.iterate' doStep
 doStep :: Coords coords => World coords -> World coords
 doStep = step adjacentCells activate
  where
-  activate :: (a1, Bool) -> [(a2, Bool)] -> Bool
-  activate (_, True) neighbors =
-    length (filter snd neighbors) `elem` [2, 3]
-  activate (_, False) neighbors =
-    length (filter snd neighbors) == 3
+  adjacentCells p = filter (/= p) $ areaAround [p]
+  activate True neighbors =
+    length (filter id neighbors) `elem` [2, 3]
+  activate False neighbors =
+    length (filter id neighbors) == 3
 
 type NeighborhoodFn coords = coords -> [coords]
-type ActivationFn coords = (coords, Bool) -> [(coords, Bool)] -> Bool
+type ActivationFn coords = Bool -> [Bool] -> Bool
 
 step :: Coords coords => NeighborhoodFn coords -> ActivationFn coords -> World coords -> World coords
 step neighborhood activate world =
-  World . WriterT.execWriter $
-    Foldable.for_ (areaAround $ activeCells world) $ \coords -> do
-      let currentlyActive = isActive world coords
-          neighbors = (id &&& isActive world) <$> neighborhood coords
-          becomesActive = activate (coords, currentlyActive) neighbors
-      when becomesActive $
-        WriterT.tell $ Set.singleton coords
+  activeCells world
+    & areaAround
+    & Maybe.mapMaybe activateCell
+    & Set.fromList
+    & World
+ where
+  activateCell coords =
+    Bool.bool Nothing (Just coords) $
+      activate
+        (isActive world coords)
+        (isActive world <$> neighborhood coords)
 
 class Ord coords => Coords coords where
-  adjacentCells :: coords -> [coords]
   areaAround :: Foldable t => t coords -> [coords]
 
 instance Coords Int where
   areaAround points
     | Foldable.null points = []
     | otherwise = [minimum points - 1 .. maximum points + 1]
-
-  adjacentCells x =
-    [x - 1, x + 1]
 
 instance Coords coords => Coords (coords, Int) where
   areaAround points =
@@ -88,13 +87,6 @@ instance Coords coords => Coords (coords, Int) where
    where
     xyzs = fst <$> Foldable.toList points
     ws = snd <$> Foldable.toList points
-
-  adjacentCells (xyz, w) =
-    [ (xyz', w + dw)
-    | xyz' <- areaAround [xyz]
-    , dw <- [-1, 0, 1]
-    , xyz' /= xyz || dw /= 0
-    ]
 
 -- printWorld3d :: World Coords3d -> IO ()
 -- printWorld3d world =
