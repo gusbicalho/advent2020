d52f6decd52868dd4aeb46b9b3123ae3be21aa9d
Gustavo Bicalho <gusbicalho@gmail.com>
2020-12-21 15:32:11 -0300

    generic solution for any number of dimensions

--- day17/Main.hs
+++ day17/Main.hs
@@ -32,19 +32,23 @@ import Data.Set qualified as Set
 
 main :: IO ()
 main = do
-  putStrLn "Part 1"
-  putStrLn . ("example: " <>) . show . solve $ example3d
+  putStrLn "Part 0 (2d)"
+  putStrLn . ("example: " <>) . show . solve $ example
   putStrLn . ("for real: " <>) . show . solve =<< input
-  putStrLn "Part 2"
-  putStrLn . ("example: " <>) . show . solve . in4d $ example3d
-  putStrLn . ("for real: " <>) . show . solve . in4d =<< input
-
--- >>> solve1 example3d
--- 112
+  putStrLn "Part 1 (3d)"
+  putStrLn . ("example: " <>) . show . solve . liftD 0 $ example
+  putStrLn . ("for real: " <>) . show . solve . liftD 0 =<< input
+  putStrLn "Part 2 (4d)"
+  putStrLn . ("example: " <>) . show . solve . liftD 0 . liftD 0 $ example
+  putStrLn . ("for real: " <>) . show . solve . liftD 0 . liftD 0 =<< input
 
 solve :: Coords coords => World coords -> Int
-solve = Set.size . activeCells . (!! 6) . List.iterate' (step adjacentCells activate)
+solve = Set.size . activeCells . (!! 6) . List.iterate' doStep
+
+doStep :: Coords coords => World coords -> World coords
+doStep = step adjacentCells activate
  where
+  activate :: (a1, Bool) -> [(a2, Bool)] -> Bool
   activate (_, True) neighbors =
     length (filter snd neighbors) `elem` [2, 3]
   activate (_, False) neighbors =
@@ -67,49 +71,29 @@ class Ord coords => Coords coords where
   adjacentCells :: coords -> [coords]
   areaAround :: Foldable t => t coords -> [coords]
 
-instance Coords Coords3d where
+instance Coords Int where
+  areaAround points
+    | Foldable.null points = []
+    | otherwise = [minimum points - 1 .. maximum points + 1]
+
+  adjacentCells x =
+    [x - 1, x + 1]
+
+instance Coords coords => Coords (coords, Int) where
   areaAround points =
-    [ (x, y, z)
-    | x <- rangeAround xs
-    , y <- rangeAround ys
-    , z <- rangeAround zs
+    [ (xyz, w)
+    | xyz <- areaAround xyzs
+    , w <- areaAround ws
     ]
    where
-    xs = (\(x, _, _) -> x) <$> Foldable.toList points
-    ys = (\(_, y, _) -> y) <$> Foldable.toList points
-    zs = (\(_, _, z) -> z) <$> Foldable.toList points
-    rangeAround ds = [minimum ds - 1 .. maximum ds + 1]
+    xyzs = fst <$> Foldable.toList points
+    ws = snd <$> Foldable.toList points
 
-  adjacentCells (x, y, z) =
-    [ (x + dx, y + dy, z + dz)
-    | dx <- [-1 .. 1]
-    , dy <- [-1 .. 1]
-    , dz <- [-1 .. 1]
-    , dx /= 0 || dy /= 0 || dz /= 0
-    ]
-
-instance Coords Coords4d where
-  areaAround points =
-    [ (x, y, z, w)
-    | x <- rangeAround xs
-    , y <- rangeAround ys
-    , z <- rangeAround zs
-    , w <- rangeAround ws
-    ]
-   where
-    xs = (\(x, _, _, _) -> x) <$> Foldable.toList points
-    ys = (\(_, y, _, _) -> y) <$> Foldable.toList points
-    zs = (\(_, _, z, _) -> z) <$> Foldable.toList points
-    ws = (\(_, _, _, w) -> w) <$> Foldable.toList points
-    rangeAround ds = [minimum ds - 1 .. maximum ds + 1]
-
-  adjacentCells (x, y, z, w) =
-    [ (x + dx, y + dy, z + dz, w + dw)
-    | dx <- [-1 .. 1]
-    , dy <- [-1 .. 1]
-    , dz <- [-1 .. 1]
-    , dw <- [-1 .. 1]
-    , dx /= 0 || dy /= 0 || dz /= 0 || dw /= 0
+  adjacentCells (xyz, w) =
+    [ (xyz', w + dw)
+    | xyz' <- areaAround [xyz]
+    , dw <- [-1, 0, 1]
+    , xyz' /= xyz || dw /= 0
     ]
 
 -- printWorld3d :: World Coords3d -> IO ()
@@ -118,47 +102,45 @@ instance Coords Coords4d where
 --     putStrLn $ "z=" <> show z
 --     Foldable.traverse_ putStrLn rows
 --     putStrLn ""
-
 -- showWorld3d :: World Coords3d -> [(Int, [String])]
 -- showWorld3d world@(World activeCells) =
 --   flip fmap (fullRange zs) $ \z ->
 --     ( z
 --     , flip fmap (fullRange ys) $ \y ->
 --         flip fmap (fullRange xs) $ \x ->
---           Bool.bool '.' '#' $ isActive world (x, y, z)
+--           Bool.bool '.' '#' $ isActive world ((x, y), z)
 --     )
 --  where
---   xs = (\(x, _, _) -> x) <$> Foldable.toList activeCells
---   ys = (\(_, y, _) -> y) <$> Foldable.toList activeCells
---   zs = (\(_, _, z) -> z) <$> Foldable.toList activeCells
+--   xs = (\((x, _), _) -> x) <$> Foldable.toList activeCells
+--   ys = (\((_, y), _) -> y) <$> Foldable.toList activeCells
+--   zs = (\((_, _), z) -> z) <$> Foldable.toList activeCells
 --   fullRange ds = [minimum ds .. maximum ds]
 
 isActive :: Ord coords => World coords -> coords -> Bool
 isActive World{activeCells} coords = coords `Set.member` activeCells
 
-type Coords3d = (Int, Int, Int)
-type Coords4d = (Int, Int, Int, Int)
+type Coords2d = (Int, Int)
 newtype World c = World {activeCells :: Set c}
   deriving stock (Eq, Show)
 
-in4d :: World Coords3d -> World Coords4d
-in4d = World . Set.map (\(x,y,z) -> (x,y,z,0)) . activeCells
+liftD :: Ord coords => Int -> World coords -> World (coords, Int)
+liftD w = World . Set.map (,w) . activeCells
 
-parseWorld3d :: Int -> Seq String -> World Coords3d
-parseWorld3d z = World . Set.fromList . Seq.foldMapWithIndex toLiveCoords
+parseWorld2d :: Seq String -> World Coords2d
+parseWorld2d = World . Set.fromList . Seq.foldMapWithIndex toLiveCoords
  where
-  toLiveCoords :: Int -> String -> [Coords3d]
+  toLiveCoords :: Int -> String -> [Coords2d]
   toLiveCoords y = Maybe.mapMaybe (toLiveCoord y) . zip [0 ..]
-  toLiveCoord y (x, '#') = Just (x, y, z)
+  toLiveCoord y (x, '#') = Just (x, y)
   toLiveCoord _ _ = Nothing
 
-example3d :: World Coords3d
-example3d =
-  parseWorld3d 0 . Seq.fromList $
+example :: World Coords2d
+example =
+  parseWorld2d . Seq.fromList $
     [ ".#."
     , "..#"
     , "###"
     ]
 
-input :: IO (World Coords3d)
-input = parseWorld3d 0 <$> Advent2020.Input.loadInput Just "day17/input"
+input :: IO (World Coords2d)
+input = parseWorld2d <$> Advent2020.Input.loadInput Just "day17/input"
