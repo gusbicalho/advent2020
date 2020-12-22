4928860a2283b20149f49a3f86144a77bbf19d79
Gustavo Bicalho <gusbicalho@gmail.com>
2020-12-22 14:38:57 -0300

    show off AST printing skillz

--- day18/Main.hs
+++ day18/Main.hs
@@ -17,9 +17,9 @@
 module Main (main) where
 
 import Advent2020.Input qualified
+import Control.Arrow (Arrow ((&&&)))
 import Data.Char qualified as Char
 import Data.Foldable qualified as Foldable
-import Data.Function ((&))
 import Data.Maybe qualified as Maybe
 import Data.Sequence (Seq)
 import Data.Sequence qualified as Seq
@@ -29,34 +29,45 @@ import Prelude hiding (exp)
 main :: IO ()
 main = do
   putStrLn "Part 1"
-  putStrLn . ("example: " <>) . show $ solve1 example
+  case (id &&& parseExp1 @AST &&& parseExp1 @Word) $ example of
+    (lexemes, (Right ast, Right result)) -> do
+      putStrLn "example:"
+      putStrLn $ "> lexemes: " <> showLexemes lexemes
+      putStrLn $ "> AST:     " <> showAST ast
+      putStrLn $ "> result:  " <> show result
+    _ -> pure ()
   putStrLn . ("for real: " <>) . show . solve1 =<< input
   putStrLn "Part 2"
-  putStrLn . ("example: " <>) . show $ solve2 example
+  case (id &&& parseExp2 @AST &&& parseExp2 @Word) $ example of
+    (lexemes, (Right ast, Right result)) -> do
+      putStrLn "example:"
+      putStrLn $ "> lexemes: " <> showLexemes lexemes
+      putStrLn $ "> AST:     " <> showAST ast
+      putStrLn $ "> result:  " <> show result
+    _ -> pure ()
   putStrLn . ("for real: " <>) . show . solve2 =<< input
 
--- >>> solve1 example
--- 26406
+-- >>> solve1 [example]
+-- 13632
 
-solve1 :: Seq (Seq Lexeme) -> Word
-solve1 = Foldable.sum . fmap (either (const 0) id . parseExp1 @Word)
+solve1 :: Foldable t => t (Seq Lexeme) -> Word
+solve1 = Foldable.sum . fmap (either (const 0) id . parseExp1 @Word) . Foldable.toList
 
 parseExp1 :: forall e t. (Foldable t, Exp e) => t Lexeme -> Either String e
 parseExp1 = fmap snd . exp . Foldable.toList
  where
-  -- ls, moreLs :: [Lexeme]
-  exp :: [Lexeme] -> Either String ([Lexeme], e)
+  exp :: ExpParser e
   exp ls = do
     (moreLs, e) <- term ls
     binaryOpTail e moreLs
-  term :: [Lexeme] -> Either String ([Lexeme], e)
+  term :: ExpParser e
   term (Number i : ls) = Right (ls, number i)
   term (LeftParen : ls) =
     exp ls >>= \case
       (RightParen : moreLs, e) -> Right (moreLs, e)
       (moreLs, _) -> Left $ "Expected RightParen, found " <> show moreLs
   term ls = Left $ "Expected LeftParen or Number, found " <> show ls
-  binaryOpTail :: e -> [Lexeme] -> Either String ([Lexeme], e)
+  binaryOpTail :: e -> ExpParser e
   binaryOpTail leftTerm (Operator op : ls) = do
     (moreLs, rightTerm) <- term ls
     binaryOpTail (evalOp op leftTerm rightTerm) moreLs
@@ -64,22 +75,23 @@ parseExp1 = fmap snd . exp . Foldable.toList
   binaryOpTail leftTerm [] = Right ([], leftTerm)
   binaryOpTail _ ls = Left $ "Expected Operator, found " <> show ls
 
-solve2 :: Seq (Seq Lexeme) -> Word
-solve2 = Foldable.sum . fmap (either (const 0) id . parseExp2 @Word)
+-- >>> solve2 [example]
+-- 23340
 
-type ExpParser e = [Lexeme] -> Either String ([Lexeme], e)
+solve2 :: Foldable t => t (Seq Lexeme) -> Word
+solve2 = Foldable.sum . fmap (either (const 0) id . parseExp2 @Word) . Foldable.toList
 
 parseExp2 :: forall e t. (Foldable t, Exp e) => t Lexeme -> Either String e
 parseExp2 = fmap snd . exp . Foldable.toList
  where
-  -- ls, moreLs :: [Lexeme]
   exp = binaryRightAssociativeOps [Times, Plus] term
   binaryRightAssociativeOps :: [Operator] -> ExpParser e -> ExpParser e
   binaryRightAssociativeOps [] termParser ls = termParser ls
   binaryRightAssociativeOps ops@(op : higherPrecedenceOps) termParser ls =
     binaryRightAssociativeOps higherPrecedenceOps termParser ls >>= \case
-      (Operator foundOp : moreLs, leftTerm) | foundOp == op ->
-        (evalOp op leftTerm <$>) <$> binaryRightAssociativeOps ops termParser moreLs
+      (Operator foundOp : moreLs, leftTerm)
+        | foundOp == op ->
+          (evalOp op leftTerm <$>) <$> binaryRightAssociativeOps ops termParser moreLs
       result -> pure result
   term :: ExpParser e
   term (Number i : ls) = Right (ls, number i)
@@ -88,6 +100,8 @@ parseExp2 = fmap snd . exp . Foldable.toList
       (RightParen : moreLs, e) -> Right (moreLs, e)
       (moreLs, _) -> Left $ "Expected RightParen, found " <> show moreLs
   term ls = Left $ "Expected LeftParen or Number, found " <> show ls
+
+type ExpParser e = [Lexeme] -> Either String ([Lexeme], e)
 
 class Exp e where
   number :: Word -> e
@@ -113,6 +127,17 @@ data AST
   | ABinOp Operator AST AST
   deriving stock (Eq, Ord, Show)
 
+showAST :: AST -> [Char]
+showAST (ANum w) = show w
+showAST (ABinOp op a b) =
+  "("
+    <> showAST a
+    <> " "
+    <> showOperator op
+    <> " "
+    <> showAST b
+    <> ")"
+
 data Operator = Plus | Times
   deriving stock (Eq, Ord, Show)
 data Lexeme
@@ -121,6 +146,18 @@ data Lexeme
   | Operator Operator
   | Number Word
   deriving stock (Eq, Ord, Show)
+
+showOperator :: Operator -> [Char]
+showOperator Plus = "+"
+showOperator Times = "*"
+
+showLexemes :: Foldable t => t Lexeme -> String
+showLexemes = unwords . fmap showLexeme . Foldable.toList
+ where
+  showLexeme LeftParen = "("
+  showLexeme RightParen = ")"
+  showLexeme (Operator op) = showOperator op
+  showLexeme (Number w) = show w
 
 parseLexemes :: String -> Maybe (Seq Lexeme)
 parseLexemes = fmap Seq.fromList . go
@@ -136,16 +173,8 @@ parseLexemes = fmap Seq.fromList . go
     Just i -> (Number i :) <$> go cs
     Nothing -> Nothing
 
-example :: Seq (Seq Lexeme)
-example =
-  Seq.fromList
-    . Maybe.mapMaybe parseLexemes
-    $ [ "1 + 2 * 3 + 4 * 5 + 6"
-      , "2 * 3 + (4 * 5)"
-      , "5 + (8 * 3 + 9 + 3 * 4 * 3)"
-      , "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"
-      , "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
-      ]
+example :: Seq Lexeme
+example = Maybe.fromJust $ parseLexemes "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"
 
 input :: IO (Seq (Seq Lexeme))
 input = Advent2020.Input.loadInput parseLexemes "day18/input"
