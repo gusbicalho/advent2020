15c341a9fb1656b84650c4ad06131bb29e26341f
Gustavo Bicalho <gusbicalho@gmail.com>
2020-12-25 21:30:42 +0000

    initial solution to day 20 part 1

--- day20/Main.hs
+++ day20/Main.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -17,6 +18,8 @@
 module Main (main) where
 
 import Advent2020.Input qualified
+import Control.Arrow (Arrow ((&&&)))
+import Control.Monad (guard, (>=>))
 import Data.Char qualified as Char
 import Data.Foldable qualified as Foldable
 import Data.Function ((&))
@@ -26,7 +29,6 @@ import Data.Maybe qualified as Maybe
 import Data.Sequence (Seq)
 import Data.Sequence qualified as Seq
 import Text.Read qualified as Read
-import Control.Monad ((>=>))
 
 main :: IO ()
 main = do
@@ -44,28 +46,43 @@ solve2 :: Seq a -> Int
 solve2 = Seq.length
 
 solveTiles :: [Tile] -> Maybe (Seq (Seq Tile))
-solveTiles tiles = toTable . verticals . fmap pure . horizontals . fmap pure $ tiles
+solveTiles tiles = do
+  (corner, _) <- List.find isCorner matchList
+  let (borderRow, remaining) = goRow (ignoringOrder matchH) [] corner (remove (sameTileIdAs corner . fst) matchList)
+  let (cols, remaining') = goCols [] borderRow remaining
+  guard (null remaining')
+  pure $ Seq.fromList $ Seq.fromList <$> cols
  where
-  squareSize = fromIntegral $ sqrtWord (List.genericLength @Word tiles)
-  emptyTable = Seq.replicate squareSize . Seq.replicate squareSize $ Nothing
-  -- Ah, fro part 1 at least we only need to find corners, not solve puzzle
-  go :: Seq (Seq (Maybe Tile)) -> [Tile] ->
-  horizontals :: [[Tile]] -> [[Tile]]
-  horizontals horizontalSpans = loopUntilStable (concatMatching matchHorizontally)
-  verticals :: [[[Tile]]] -> [[[Tile]]]
-  verticals = _
-  toTable :: [[[Tile]]] -> Maybe (Seq (Seq Tile))
-  toTable [square] = Just . Seq.fromList $ Seq.fromList <$> square
-  toTable _ = Nothing
-
-
-sqrtWord :: Word -> Word
-sqrtWord 0 = 0
-sqrtWord 1 = 1
-sqrtWord n = go 2
- where
-  go r | r * r >= n = r
-       | otherwise = go (r + 1)
+  remove p = filter (not . p)
+  sameTileIdAs t = (== tileId t) . tileId
+  goRow matcher rowTiles headTile possibleTiles =
+    case Maybe.mapMaybe (checkForMatch matcher headTile) possibleTiles of
+      [] -> (reverse $ headTile : rowTiles, possibleTiles)
+      (t : _) -> goRow matcher (headTile : rowTiles) t (remove (sameTileIdAs t . fst) possibleTiles)
+  checkForMatch matcher t (t2, (t2up, t2right, t2down, t2left)) =
+    if any (sameTileIdAs t) $ Maybe.catMaybes [t2up, t2right, t2down, t2left]
+      then lookForMatch matcher t t2
+      else Nothing
+  goCols acc [] possibleTiles = (acc, possibleTiles)
+  goCols acc (colHead : moreColHeads) possibleTiles =
+    let (col, remaining) = goRow (ignoringOrder matchV) [] colHead possibleTiles
+     in goCols (col : acc) moreColHeads remaining
+  isCorner (_, (up, right, down, left)) =
+    (Maybe.isJust up /= Maybe.isJust down) && (Maybe.isJust left /= Maybe.isJust right)
+  matchList :: [(Tile, (Maybe Tile, Maybe Tile, Maybe Tile, Maybe Tile))]
+  matchList = fmap (id &&& matchesFor) tiles
+  ignoringOrder matcher t1 t2 = matcher t1 t2 || matcher t2 t1
+  matchesFor tile =
+    ( Maybe.listToMaybe $ Maybe.mapMaybe (lookForMatch (flip matchV) tile) $ remove (sameTileIdAs tile) tiles
+    , Maybe.listToMaybe $ Maybe.mapMaybe (lookForMatch matchH tile) $ remove (sameTileIdAs tile) tiles
+    , Maybe.listToMaybe $ Maybe.mapMaybe (lookForMatch matchV tile) $ remove (sameTileIdAs tile) tiles
+    , Maybe.listToMaybe $ Maybe.mapMaybe (lookForMatch (flip matchH) tile) $ remove (sameTileIdAs tile) tiles
+    )
+  lookForMatch matcher tg1 tg2 =
+    Maybe.listToMaybe $ do
+      tg2' <- flips tg2
+      guard (matcher tg1 tg2')
+      pure tg2'
 
 fourCorners :: Seq (Seq a) -> [a]
 fourCorners = firstAndLast >=> firstAndLast
@@ -73,31 +90,55 @@ fourCorners = firstAndLast >=> firstAndLast
   firstAndLast (f Seq.:<| (_ Seq.:|> l)) = [f, l]
   firstAndLast _ = []
 
-flips :: Tile -> [Tile]
-flips tile = fmap ($ tile) [flipX, flipY, rotateCw, rotate180, rotateCcw]
+instance Piece Tile where
+  matchH Tile{tileData = td1} Tile{tileData = td2} =
+    Maybe.mapMaybe seqLast (Foldable.toList td1) == Maybe.mapMaybe seqFirst (Foldable.toList td2)
+   where
+    seqLast (_ Seq.:|> l) = Just l
+    seqLast _ = Nothing
+    seqFirst (f Seq.:<| _) = Just f
+    seqFirst _ = Nothing
 
-flipX :: Tile -> Tile
-flipX tile@Tile { tileData } = tile { tileData = fmap Seq.reverse tileData }
+  matchV Tile{tileData = td1} Tile{tileData = td2} =
+    seqLast td1 == seqFirst td2
+   where
+    seqLast (_ Seq.:|> l) = Just l
+    seqLast _ = Nothing
+    seqFirst (f Seq.:<| _) = Just f
+    seqFirst _ = Nothing
 
-flipY :: Tile -> Tile
-flipY tile@Tile { tileData } = tile { tileData = Seq.reverse tileData }
+  flipX tile@Tile{tileData} = tile{tileData = fmap Seq.reverse tileData}
 
-transpose :: Tile -> Tile
-transpose tile@Tile{tileData} = tile { tileData = transposed }
- where
-  transposed =
-    Seq.fromList . flip fmap [0..9] $ \y ->
-      Seq.fromList . flip fmap [0..9] $ \x ->
-        tileData & (Seq.!? x) >>= (Seq.!? y) & Maybe.fromMaybe False
+  flipY tile@Tile{tileData} = tile{tileData = Seq.reverse tileData}
 
-rotate180 :: Tile -> Tile
+  transpose tile@Tile{tileData} = tile{tileData = transposed}
+   where
+    transposed =
+      Seq.fromList . flip fmap [0 .. 9] $ \y ->
+        Seq.fromList . flip fmap [0 .. 9] $ \x ->
+          tileData & (Seq.!? x) >>= (Seq.!? y) & Maybe.fromMaybe False
+
+class Piece p where
+  matchH :: p -> p -> Bool
+  matchV :: p -> p -> Bool
+  flipX :: p -> p
+  flipY :: p -> p
+  transpose :: p -> p
+
+flips :: Piece p => p -> [p]
+flips tile = fmap ($ tile) [id, flipX, flipY, rotateCw, rotate180, rotateCcw, transpose, transposeFlipped]
+
+transposeFlipped :: Piece p => p -> p
+transposeFlipped = rotate180 . transpose
+
+rotate180 :: Piece p => p -> p
 rotate180 = flipX . flipY
 
-rotateCw :: Tile -> Tile
-rotateCw  = flipX . transpose
+rotateCw :: Piece p => p -> p
+rotateCw = flipX . transpose
 
-rotateCcw :: Tile -> Tile
-rotateCcw  = flipY . transpose
+rotateCcw :: Piece p => p -> p
+rotateCcw = flipY . transpose
 
 data Tile = Tile {tileId :: Word, tileData :: Seq (Seq Bool)}
   deriving stock (Eq, Ord, Show)
@@ -240,4 +281,4 @@ example =
     ]
 
 input :: IO (Seq Tile)
-input = Seq.fromList . parseTiles <$> Advent2020.Input.loadInput Just "dayTemplate/input"
+input = Seq.fromList . parseTiles <$> Advent2020.Input.loadInput Just "day20/input"
